ce39c73ce32dd08e3dec4ecbdd774e4476ce4e28
Nugaev <sardor0418@mail.ru>
2025-08-06 09:55:11 +0500

    123

--- controllers/TelegramController.php
+++ controllers/TelegramController.php
@@ -2,53 +2,14 @@
 
 namespace controllers;
 
+use Telegram\Bot\Api;
+
 class TelegramController
-{
-    public $url;
-    public function __construct()
+{    public function bot()
     {
-        $token = '8297930277:AAEeX9D0hmwxJdlDu7wtVXQ0dpHGzqrbCAw';
-        $this->url = 'https://api.telegram.org/bot' . $token . '/';
-    }
+        $telegram = new Api('8297930277:AAEeX9D0hmwxJdlDu7wtVXQ0dpHGzqrbCAw');
 
-    public function bot()
-    {
-        $content = file_get_contents("php://input");
-        $update = json_decode($content, true);
-
-        // Xabar tafsilotlari
-        $chat_id = $update['message']['chat']['id'] ?? null;
-        $text = $update['message']['text'] ?? '';
-
-        // Javob yuborish
-        if ($chat_id && $text) {
-            $text = 'Salom bu telegram rasmi';
-            $this->sendPhoto($chat_id, $text, 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/250px-Telegram_2019_Logo.svg.png');
-        }
-    }
-
-    public function sendMessage($chat_id, $text)
-    {
-        $data = [
-            'chat_id' => $chat_id,
-            'text' => $text,
-            'parse_mode' => 'HTML'
-        ];
-        $url = "{$this->url}sendMessage?" . http_build_query($data);
-
-        file_get_contents($url);
-    }
-
-    public function sendPhoto($chat_id, $text, $photo)
-    {
-        $data = [
-            'chat_id' => $chat_id,
-            'caption' => $text,
-            'photo' => $photo,
-            'parse_mode' => 'HTML'
-        ];
-        $url = "{$this->url}sendPhoto?" . http_build_query($data);
-
-        file_get_contents($url);
+        // Example usage
+        $response = $telegram->getMe();
     }
 }
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit00a4348b1bce0a1545a9e8c17e1dac05
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit00a4348b1bce0a1545a9e8c17e1dac05::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
