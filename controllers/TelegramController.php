3a19cdfc5c2d442b3dca515ebeb0653301a318a3
Nugaev <sardor0418@mail.ru>
2025-08-06 09:43:49 +0500

    work with webhook

--- controllers/TelegramController.php
+++ controllers/TelegramController.php
@@ -13,12 +13,8 @@ class TelegramController
 
     public function bot()
     {
-
-
-
         $content = file_get_contents("php://input");
         $update = json_decode($content, true);
-
 
         // Xabar tafsilotlari
         $chat_id = $update['message']['chat']['id'] ?? null;
@@ -26,10 +22,8 @@ class TelegramController
 
         // Javob yuborish
         if ($chat_id && $text) {
-            $reply = '<a href="tg://user?id=3673579">Sardor Nugaev</a>';
-            $this->sendMessage($chat_id, $reply);
-
-            $this->sendMessage($chat_id, 'Bu ikkinchi message');
+            $text = 'Salom bu telegram rasmi';
+            $this->sendPhoto($chat_id, $text, 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/250px-Telegram_2019_Logo.svg.png');
         }
     }
 
@@ -41,6 +35,19 @@ class TelegramController
             'parse_mode' => 'HTML'
         ];
         $url = "{$this->url}sendMessage?" . http_build_query($data);
+
+        file_get_contents($url);
+    }
+
+    public function sendPhoto($chat_id, $text, $photo)
+    {
+        $data = [
+            'chat_id' => $chat_id,
+            'caption' => $text,
+            'photo' => $photo,
+            'parse_mode' => 'HTML'
+        ];
+        $url = "{$this->url}sendPhoto?" . http_build_query($data);
 
         file_get_contents($url);
     }
